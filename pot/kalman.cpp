6dd90bfe0c1d69b160c4acca371855093455fe6e
Dmitriy <skull18@ngs.ru>
2019-10-31 01:19:05 +0700

    [~] refactored project structure [+] added setup command for arduino [+] added reset action for arduino [+] added humidity for arduino [+] added at commands when access from terminal [!] arduino software is ready

---
+++ pot/kalman.cpp
@@ -0,0 +1,29 @@
+// easiest kalman's filter realization
+class Kalman {
+  private:
+    float deviation = 0.25;
+    float coeff = 0.05;
+    float pc = 0.0;
+    float g = 0.0;
+    float p = 1.0;
+    float xp = 0.0;
+    float zp = 0.0;
+    float xe = 0.0;
+  public:
+    Kalman() {
+      return;
+    }
+    Kalman(float dev, float c) {
+      this->deviation = dev;
+      this->coeff = c;
+    }
+    float filter(float val) {
+      this->pc = this->p + this->coeff;
+      this->g = this->pc / (this->pc + this->deviation);
+      this->p = (1.0 - this->g) * this->pc;
+      this->xp = this->xe;
+      this->zp = this->xp;
+      this->xe = this->g * (val - this->zp) + this->xp;
+      return this->xe;
+    }
+};
