57050a3f132f19bd863c13a5564651d927270f4c
Dmitriy <skull18@ngs.ru>
2019-11-20 01:59:33 +0700

    arduino bugfix and new raspberry software [~] fixed bt restart for arduino [+] added tags for logs [+] added exceptions in log [~] bt socket uses were secured [~] all logs were moved to log folder [+] added setup event [~] reworked event execution to deal with every event [~] device history now in list [+] added device socket locks [+] added some sleeps for proper hc06 work [+] added time in log [+] added http requests log [+] added control panel web page [~] api on index.html was moved to api.html [~] objects required in web pages were simplified [~] small css fixes

--- hub/Events.py
+++ hub/Events.py
@@ -4,13 +4,12 @@ from time import time, sleep
 from datetime import datetime as dt
 
 class Event:
-	def __init__(self, master, dev, time=0, repeat=None, idx=0):
+	def __init__(self, master, dev, time=0, repeat=None):
 		self.master = master # link to hub
 		self.dev = dev # device
 		self.time = int(time) # unix timestamp
 		self.repeat = repeat # unix timestamp
 		self.started = False # start flag
-		self.id = idx # unique id
 		self.name = self.__class__.__name__.lower() # event's name
 
 	def __getitem__(self, key):
@@ -21,57 +20,86 @@ class Event:
 
 	def __str__(self):
 		n = self.name.capitalize()
-		t = dt.fromtimestamp(self.time)
+		t = dt.fromtimestamp(self.time).strftime("%H:%M:%S %d/%m/%Y")
 		d = str(self.dev)
 		return "%s event on %s for %s" % (n, t, d)
 
-	def exec(self):
+	def exec(self, idx):
 		now = time() # get current unix timestamp
 		if now >= self.time and self.started is False:
-			Logger().write("[!] %s is starting" % str(self))
 			self.started = True
-			Thread(target=self.threadFunc).start()
-			self.master.removeEvent(self.id)
+			Logger().write("[!] %s is starting" % str(self), tag="EVENT")
+			Thread(target=self.threadFunc, args=(idx,)).start()
 
 	def func(self):
 		pass
 
-	def threadFunc(self):
-		self.dev.connect()
-		if self.dev["sock"] is not None:
-			self.func()
-		self.dev.close()
-		Logger().write("[!] %s was finished" % str(self))
+	def threadFunc(self, idx):
+		ret = self.func()
+
+		self.master.removeEvent(idx)
+		self.started = False
+
+		now = int(time())
+		Logger().write("[!] %s was %s" % (str(self), "finished" if ret is True else "failed"), tag="EVENT")
+		if self.master.findDeviceID(self.dev["mac"]) is None:
+			return
+
+		if ret is False:
+			newTime = now + 10
+			self.master.addEvent(self.name, self.dev, newTime)
 
 		if self.repeat is not None:
-			now = time()
 			newTime = self.time + int(self.repeat)
 			self.time = newTime if newTime > now else now + int(self.repeat)
 			self.master.addEvent(self.name, self.dev, self.time, self.repeat)
-		self.started = False
+
+class Setup(Event):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+	def func(self):
+		self.repeat = None
+		return self.dev.setup()
 
 class Refresh(Event):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
 	def func(self):
-		self.dev.send("get")
+		self.dev.connect()
+		ret = self.dev.send("get")
 		out = self.dev.recv()
-		if out is None:
-			return
+		self.dev.close()
+
+		if ret == -1 or out is None:
+			return False
 
 		res = { "time": int(time()) }
 		for x in out.lower().split("|"):
 			key, value = x.split(":")
 			res[key] = float(value)
 
-		self.dev.history[res["time"]] = res
+		self.dev.history.append(res)
+		return True
 
 class Water(Event):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
 	def func(self):
-		self.dev.send("set90")
+		self.dev.connect()
+		r1 = self.dev.send("set90")
 		sleep(3)
-		self.dev.send("set0")
+		r2 = self.dev.send("set0")
+		self.dev.close()
+
+		if -1 in (r1,r2):
+			return False
+		return True
+
+event_types = {
+	"setup": Setup,
+	"refresh": Refresh,
+	"water": Water
+}
--- hub/Logger.py
+++ hub/Logger.py
@@ -4,29 +4,25 @@ def singleton(cls):
 	instances = {}
 
 	def getinstance(*args, **kwargs):
-		name = args[0] if len(args) != 0 else "None"
 		if cls not in instances:
-			instances[cls] = {}
-		if name not in instances[cls]:
-			instances[cls][name] = cls()
-		return instances[cls][name]
+			instances[cls] = cls(*args, **kwargs)
+		return instances[cls]
 
 	return getinstance
 
 @singleton
 class Logger:
 	def __init__(self, *args, **kwargs):
-		self.fname = "out.log"
-		if len(args) != 0:
-			self.open(args[0])
+		fname = "logs/%s %s.log" % (args[0] if len(args) != 0 else "out", dt.now().strftime("%H-%M %d-%m-%Y"))
+		self.open(fname)
 
 	def open(self, fname):
 		self.fname = fname
 		with open(self.fname, "a") as f:
-			f.write("\n\n\t\t\t[!] NEW SESSION (%s)" % dt.now().ctime())
+			f.write("\n\n\t\t[!] NEW SESSION (%s)\n" % dt.now().ctime())
 
-	def write(self, *args):
-		msg = "".join(str(x) for x in args)
+	def write(self, *args, tag="INFO"):
+		msg = "[%s] [%s]\t%s" % (dt.now().strftime("%H:%M:%S %d/%m/%Y"), tag, "".join(str(x) for x in args))
 		with open(self.fname, "a") as f:
 			f.write(msg + "\n")
 		print(msg)
